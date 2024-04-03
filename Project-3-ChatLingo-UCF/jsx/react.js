d6fff0042c98e4dcebf8c6790d4aa97d88b9c6a3
Brodrigo04 <knightarmy350@gmail.com>
2024-04-02 20:09:59 -0400

    added react

---
+++ Project-3-ChatLingo-UCF/jsx/react.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+
+const ChatApp = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
+  const [targetLanguage, setTargetLanguage] = useState('es'); // default target language: Spanish
+
+  useEffect(() => {
+    // Function to translate text using Google Translate API
+    const translateText = async () => {
+      const apiKey = 'AIzaSyBxlhgsdvPlg3dKaBYv9xxlzEUAik6Wn0A';
+      const response = await fetch(
+        `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            q: inputText,
+            target: targetLanguage,
+          }),
+        }
+      );
+      const data = await response.json();
+      setTranslatedText(data.data.translations[0].translatedText);
+    };
+
+    // Call translateText function whenever inputText or targetLanguage changes
+    if (inputText) {
+      translateText();
+    }
+  }, [inputText, targetLanguage]);
+
+  const handleMessageSend = () => {
+    setMessages([...messages, translatedText]);
+    setInputText('');
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <select
+        value={targetLanguage}
+        onChange={(e) => setTargetLanguage(e.target.value)}
+      >
+        <option value="es">Spanish</option>
+        <option value="fr">French</option>
+        <option value="de">German</option>
+        {/* Add more languages as needed */}
+      </select>
+      <button onClick={handleMessageSend}>Send</button>
+    </div>
+  );
+};
+
+export default ChatApp;
